7da948dc3a9b46dbd5779bdf2a86d30acf8d1ba2
Vaibhav <vaibhavgupta.v890@gmail.com>
2026-02-03 17:47:01 +0530

    Initial Commit

---
+++ signaldesk/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  signaldesk
+//
+//  Created by KIET9 on 03/02/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
